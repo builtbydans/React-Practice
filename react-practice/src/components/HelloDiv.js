93d988f61a0118e4077ab9c602905325798f27f9
Danish Shafi <danish.shafi@live.co.uk>
2023-02-28 15:39:41 +0000

    second activity complete

---
+++ react-practice/src/components/HelloDiv.js
@@ -0,0 +1,14 @@
+function HelloDiv() {
+  return(
+    <div>
+      <h1>Hello! My name is Dan!</h1>
+      <ul>
+        <li>Food</li>
+        <li>Gaming</li>
+        <li>Sleep</li>
+      </ul>
+    </div>
+  );
+}
+
+export default HelloDiv;
